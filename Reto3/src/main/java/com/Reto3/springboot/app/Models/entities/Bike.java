a1fd7bc28b3f959e9d194f8f402ff5c3fe2dae92
sebastiangp97 <juansgarcia97@gmail.com>
2021-10-21 07:28:26 -0500

    Controler de BIke

--- Reto3/src/main/java/com/Reto3/springboot/app/Models/entities/Bike.java
+++ Reto3/src/main/java/com/Reto3/springboot/app/Models/entities/Bike.java
@@ -47,6 +47,57 @@ public class Bike implements Serializable {
     @OneToMany(cascade = {CascadeType.PERSIST}, mappedBy = "bike")
     @JsonIgnoreProperties({"bike","messages"})
     public List<Reservation> reservations;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    
     
 }
 
